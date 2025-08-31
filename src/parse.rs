c6fe6a888775c8d82d1d316a4dba0cd76dcfe0d8
chris <cmorrison82z@proton.me>
2025-08-31 17:51:11 +0100

    add: XmlRef

--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,7 @@
 use nom::{
     branch::alt,
     bytes::complete::{escaped, is_not, tag, take_till1, take_while, take_while1},
-    character::complete::{char, one_of},
+    character::complete::{char, one_of, multispace0},
     combinator::{cut, map, opt, value},
     error::{context, ContextError, ParseError},
     multi::{many0, many1, separated_list0},
@@ -12,10 +12,6 @@ use std::str;
 use std::{collections::HashMap, ops::Deref};
 
 use crate::types::*;
-
-fn whitespace<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
-    take_while(move |c| " \t\r\n".contains(c))(i)
-}
 
 fn xml_key<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
     take_while1(|c: char| c.is_alphanumeric() || "_-".contains(c))(i)
@@ -54,9 +50,9 @@ fn attribute_key_value<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     i: &'a str,
 ) -> IResult<&'a str, (&'a str, &'a str), E> {
     separated_pair(
-        preceded(whitespace, xml_key),
-        cut(preceded(whitespace, char('='))),
-        cut(preceded(whitespace, attribute_value)),
+        preceded(multispace0, xml_key),
+        cut(preceded(multispace0, char('='))),
+        cut(preceded(multispace0, attribute_value)),
     )
     .parse(i)
 }
@@ -65,7 +61,7 @@ fn xml_value<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     i: &'a str,
 ) -> IResult<&'a str, Xml, E> {
     preceded(
-        whitespace,
+        multispace0,
         alt((map(xml_text, |s| Xml::Text(s.into())), element)),
     )
     .parse(i)
@@ -98,14 +94,14 @@ fn opening_tag<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
         tuple((
             char('<'),
             map(
-                separated_pair(xml_key, whitespace, attributes_hash),
+                separated_pair(xml_key, multispace0, attributes_hash),
                 |(value, attributes)| {
                     let value: String = value.into();
                     Tag { value, attributes }
                 },
             ),
             preceded(
-                whitespace,
+                multispace0,
                 alt((
                     value(true, tag("/>")),  // Detect self-closing tags
                     value(false, char('>')), // Regular tags
@@ -130,7 +126,7 @@ fn element<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
             terminated(
                 many0(xml_value),
                 preceded(
-                    whitespace,
+                    multispace0,
                     delimited(tag("</"), tag(my_tag.value.clone().deref()), char('>')),
                 ),
             ),
@@ -148,16 +144,107 @@ fn element<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     }
 }
 
+fn xml_value_ref<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
+    i: &'a str,
+) -> IResult<&'a str, XmlRef<'a>, E> {
+    preceded(
+        multispace0,
+        alt((map(xml_text, |s| XmlRef::Text(s)), element_ref)),
+    )
+    .parse(i)
+}
+
+// TODO:
+// Return Iterator<(key, val)> ?
+fn attributes_hash_ref<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
+    i: &'a str,
+) -> IResult<&'a str, HashMap<&'a str, &'a str>, E> {
+    context(
+        "map",
+        map(
+            separated_list0(many1(one_of(" \t\r\n")), attribute_key_value),
+            |tuple_vec| {
+                tuple_vec
+                    .into_iter()
+                    .map(|(k, v)| (k, v))
+                    .collect()
+            },
+        ),
+    )(i)
+}
+
+// NOTE:
+// This will return the tag.
+// We can use the Tag.value to determien the `closing_tag`
+fn opening_tag_ref<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
+    i: &'a str,
+) -> IResult<&'a str, (TagRef<'a>, bool), E> {
+    map(
+        tuple((
+            char('<'),
+            map(
+                separated_pair(xml_key, multispace0, attributes_hash_ref),
+                |(value, attributes)| {
+                    TagRef { value, attributes }
+                },
+            ),
+            preceded(
+                multispace0,
+                alt((
+                    value(true, tag("/>")),  // Detect self-closing tags
+                    value(false, char('>')), // Regular tags
+                )),
+            ),
+        )),
+        |(_, tag, is_self_closing)| (tag, is_self_closing),
+    )(i)
+}
+
+fn element_ref<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
+    i: &'a str,
+) -> IResult<&'a str, XmlRef<'a>, E> {
+    let (remaining, (my_tag, is_self_closing)) = opening_tag_ref(i)?;
+
+    if is_self_closing {
+        Ok((remaining, XmlRef::Element(my_tag, None)))
+    } else {
+        map(
+            terminated(
+                many0(xml_value_ref),
+                preceded(
+                    multispace0,
+                    delimited(tag("</"), tag(my_tag.value), char('>')),
+                ),
+            ),
+            // TODO:
+            // Avoid cloning `attributes`.
+            // Try :
+            //  - FnOnce
+            //  - A different `map` function that is `FnOnce`
+            move |vs| {
+                XmlRef::Element(
+                    TagRef {
+                        value: my_tag.value,
+                        attributes: my_tag.attributes.clone(),
+                    },
+                    Some(vs),
+                )
+            },
+        )
+        .parse(remaining)
+    }
+}
+
 pub fn doc_type<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     i: &'a str,
 ) -> IResult<&'a str, &'a str, E> {
-    delimited(preceded(whitespace, tag("<!DOCTYPE")), is_not("?>"), char('>'))(i)
+    delimited(preceded(multispace0, tag("<!DOCTYPE")), is_not("?>"), char('>'))(i)
 }
 
 pub fn xml_meta<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     i: &'a str,
 ) -> IResult<&'a str, &'a str, E> {
-    delimited(preceded(whitespace, tag("<?")), is_not("?>"), tag("?>"))(i)
+    delimited(preceded(multispace0, tag("<?")), is_not("?>"), tag("?>"))(i)
 }
 
 // TODO:
@@ -167,6 +254,15 @@ pub fn root<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
 ) -> IResult<&'a str, Xml, E> {
     cut(preceded(
         many0(alt((xml_meta, doc_type))),
-        delimited(opt(whitespace), element, opt(whitespace)),
+        delimited(opt(multispace0), element, opt(multispace0)),
+    ))(i)
+}
+
+pub fn root_ref<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
+    i: &'a str,
+) -> IResult<&'a str, XmlRef<'a>, E> {
+    cut(preceded(
+        many0(alt((xml_meta, doc_type))),
+        delimited(opt(multispace0), element_ref, opt(multispace0)),
     ))(i)
 }
